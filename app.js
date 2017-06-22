0a99ef74fdcca8d0f8290b65f8d344410c969882
rm-bergmann <info@stereofx.net>
2017-06-22 01:01:04 +0100

    Add server updates

--- app.js
+++ app.js
@@ -4,18 +4,13 @@ const app     = express();
 
 const React    = require('react');
 const ReactDOM = require('react-dom/server');
-
-//import { match, RouterContext } from 'react-router';
-//const routes = require('./src/components/Routes.jsx').routes;
-
-//const request  = require('request');
-//const blogData = 'http://blog.dev/blog';
-
-//const ReactDOMServer = require('react-dom/server');
-//const BlogList       = require('./src/components/BlogList.jsx');
+const Home     = require('./src/components/Home.jsx');
 
 require('babel-core/register')({
-  presets: ['react']
+  presets: [
+    'react',
+    'transform-react-jsx'
+  ]
 });
 
 app.set('views', __dirname + '/src/views');
@@ -25,27 +20,17 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  
-  const Home   = require('./src/components/Home.jsx');
+
   const output = ReactDOM.renderToString(React.createFactory(Home));
 
   res.render('index',
     {
       query : req.query,
-      title : 'Home Page'
-    },
-    output
+      title : 'Home Page',
+      react : output
+    }
   );
 });
-
-
-/*
-var ComponentFactory = React.createFactory(BlogList);
-app.get('/blog', function (req, res) {
-  var markup = ReactDOMServer.renderToString(ComponentFactory());
-  res.send(markup);
-});
-*/
 
 app.get('/about', function (req, res) {
   res.send('<h1>About Page</h1>');
@@ -57,7 +42,6 @@ app.get('/about/:name?/:title?', function(req, res) {
   res.send('<h1>' + name + '</h1>' + title);
 });
 
-
 app.get('/blog/:id?', function (req, res) {
   var blogId = req.params.id;
   res.send(blogId);
@@ -67,16 +51,5 @@ app.get('*', function(req, res) {
   res.send('404 Not found');
 });
 
-
 app.listen(port);
 console.log('server started on port ' + port);
-
-
-
-
-
-
-
-
-
-
