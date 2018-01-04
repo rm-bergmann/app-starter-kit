e82ed2bda6021f802f08be55fe9f0b1f851a955c
rick <info@stereofx.net>
2018-01-04 10:23:19 +0000

    Fix server error issue after reload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path    = require('path');
 const express = require('express');
 const port    = process.env.PORT || 3000;
 const app     = express();
@@ -13,11 +14,13 @@ require('babel-core/register')({
   ]
 });
 
-app.set('views', __dirname + '/src/views');
-app.set('view engine', 'ejs');
-
 // serve static assets normally
 app.use(express.static(__dirname + '/public'));
+
+
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.get('/', function(req, res) {
 
@@ -30,25 +33,6 @@ app.get('/', function(req, res) {
       react : output
     }
   );
-});
-
-app.get('/about', function (req, res) {
-  res.send('<h1>About Page</h1>');
-});
-
-app.get('/about/:name?/:title?', function(req, res) {
-  var name = req.params.name;
-  var title = req.params.title;
-  res.send('<h1>' + name + '</h1>' + title);
-});
-
-app.get('/blog/:id?', function (req, res) {
-  var blogId = req.params.id;
-  res.send(blogId);
-});
-
-app.get('*', function(req, res) {
-  res.send('404 Not found');
 });
 
 app.listen(port);
