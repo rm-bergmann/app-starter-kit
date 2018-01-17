97803f13cb28f1060ed551b864255ca9c84c7bb1
rick <info@stereofx.net>
2018-01-17 22:09:49 +0000

    Add new updates and dependencies

---
+++ webpack.dev.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const merge   = require('webpack-merge');
+const common  = require('./webpack.common.js');
+const path    = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'public');
+
+const historyApiFallback = require('connect-history-api-fallback');
+
+const BrowserSync = require('browser-sync-webpack-plugin');
+const hostname = 'localhost';
+
+let config = merge(common, {
+  
+  devtool: 'inline-source-map',
+  
+  devServer: {
+    contentBase: './public'
+  },
+  
+  plugins: [
+  
+    new BrowserSync (
+      {
+        host: hostname,
+        port: 3000,
+        open: false,
+        server: {
+          baseDir: [BUILD_DIR],
+          middleware: [historyApiFallback()]
+        },
+        files: [
+          '*.css'
+        ],
+        plugins: [
+          {
+            module: 'bs-html-injector',
+            options: {
+              files: ['/public/*.html', '/public/bundles/*.css']
+            }
+          }
+        ]
+      }
+    ),
+    
+    new webpack.HotModuleReplacementPlugin()
+  ]
+});
+
+module.exports = config;
