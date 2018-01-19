64e24a90326a6617e36ab882a4af9ac1bb615b4e
rick <info@stereofx.net>
2018-01-19 00:03:53 +0000

    Update docs and fix linting errors

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 
-class About extends React.Component {
-  render() {
-    return (
-      <div className="content">
-        <div className="block-group">
-          <h1>About Page</h1>
-          <p>More content to go on the about page</p>
-        </div>
-      </div>
-    );
-  }
-}
+const About = () => (
+  <div className="block-group">
+    <h1>About Page</h1>
+    <p>More content to go on the about page</p>
+  </div>
+);
 
 export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="content">
-        <div className="block-group">
-          <h1>Home Page</h1>
-          <p>Some random Home page content</p>
-        </div>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div className="content">
+    <div className="block-group">
+      <h1>Home Page</h1>
+      <p>Some random Home page content</p>
+    </div>
+  </div>
+);
 
 export default Home;
 
