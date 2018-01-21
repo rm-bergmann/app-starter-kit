3b998231a2a015563cbbb77fe764cfda4a8ae414
rick <info@stereofx.net>
2018-01-21 12:06:57 +0000

    Fix route links

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
+import PropTypes from 'prop-types';
 import Dashboard from './Dashboard';
 import About from './About';
 import Blog from './Blog';
@@ -8,7 +8,6 @@ import Blog from './Blog';
 class Routes extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       active: 'active',
     };
@@ -19,18 +18,23 @@ class Routes extends React.Component {
         <div>
           <nav>
             <ul className="block-group">
-              <li className={this.state.active}><Link to={Dashboard}>Dashboard</Link></li>
-              <li><Link to={About}>About</Link></li>
-              <li><Link to={Blog}>Blog</Link></li>
+              <li className={this.state.active}><Link to="/">Dashboard</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/blog">Blog</Link></li>
             </ul>
           </nav>
           <Route exact path="/" component={Dashboard} />
           <Route path="/about" component={About} />
           <Route path="/blog" component={Blog} />
+          {this.props.children}
         </div>
       </Router>
     );
   }
 }
+
+Routes.propTypes = {
+  children: PropTypes.string.isRequired,
+};
 
 export default Routes;
