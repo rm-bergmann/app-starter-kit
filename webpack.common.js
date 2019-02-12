561c48f4e8f06c06f7ac2f996aef53f96d430fb6
Ricky Bergmann <ricky.bergmann@mvfglobal.com>
2019-02-12 00:17:04 +0000

    Fix webpack production build

--- webpack.common.js
+++ webpack.common.js
@@ -54,21 +54,4 @@ module.exports = {
       },
     },
   },
-
-  /*
-  plugins: [
-    
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new ManifestPlugin(),
-    
-    new CleanWebpackPlugin([
-      'public/bundles/*.hot-update.*'
-    ]),
-    
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-  ]
-  */
 };
