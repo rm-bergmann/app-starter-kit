771f1a8d35b96734e526f74d11c1021281e17404
Ricky Bergmann <ricky.bergmann@mvfglobal.com>
2019-02-17 22:22:25 +0000

    Add new components

---
+++ src/components/Examples/Examples.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Examples = () => (
+  <div className="content content-examples">
+    <h1 className="content-examples--title">Title Large</h1>
+    <input className="content-examples--input" />
+    <button type="button" className="content-examples--button">
+      Button
+    </button>
+  </div>
+);
+
+export default Examples;
